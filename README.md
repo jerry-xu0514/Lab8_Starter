ea7650defd961ad3fb931048f4dfb1ae069895bc
Jerry Xu <jerriven@gmail.com>
2022-11-21 20:23:53 -0800

    updated unit testing and readme

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Lab 8 - Starter
 
 
-I think I would choose "Within a Github action that runs whenever code is pushed", because this way we can automate the process and reduce the workload that is handled by each individual software developer. The problems would be easy for other co-workers to see and understand. 
+1. I think I would choose "Within a Github action that runs whenever code is pushed", because this way we can automate the process and reduce the workload that is handled by each individual software developer. The problems would be easy for other co-workers to see and understand. 
+2. No, this would be done using unit tests instead. Because end-to-end tests would not test for function outputs.
+3. no, because a unit test does not test for the interactions that occurs between components on an application or feature level.
+4. yes, because unit testing tests for the outputs of smaller functions of the bigger program.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,97 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+
+test("is number 1", () => {
+    expect(functions.isPhoneNumber("111-111-1111")).toBe(true);
+})
+test("is number 2", () => {
+    expect(functions.isPhoneNumber("222-222-2222")).toBe(true);
+})
+test("is number 3", () => {
+    expect(functions.isPhoneNumber("hi")).toBe(false);
+})
+test("is number 4", () => {
+    expect(functions.isPhoneNumber("1111111111")).toBe(false);
+})
+
+test("is Email 1", ()=>
+{
+    expect(functions.isEmail("123@gmail.com")).toBe(true);
+})
+
+test("is Email 2", ()=>
+{
+    expect(functions.isEmail("123@hihi.com")).toBe(true);
+})
+
+test("is Email3", ()=>
+{
+    expect(functions.isEmail("123@gmail")).toBe(false);
+})
+
+test("is Email 4", ()=>
+{
+    expect(functions.isEmail("1")).toBe(false);
+})
+
+test("is Strong 1", ()=>
+{
+    expect(functions.isStrongPassword("AbCdEf1029")).toBe(true);
+})
+
+test("is Strong 2", ()=>
+{
+    expect(functions.isStrongPassword("myPassword")).toBe(true);
+})
+
+test("is Strong 3", ()=>
+{
+    expect(functions.isStrongPassword("123@Gmail")).toBe(false);
+})
+
+test("is Strong 4", ()=>
+{
+    expect(functions.isStrongPassword("1")).toBe(false);
+})
+
+test("is hexColor 1", ()=>
+{
+    expect(functions.isHexColor("#FFFFFF")).toBe(true);
+})
+
+test("is hexColor 2", ()=>
+{
+    expect(functions.isHexColor("#000000")).toBe(true);
+})
+
+test("is hexColor 3", ()=>
+{
+    expect(functions.isHexColor("123@Gmail")).toBe(false);
+})
+
+test("is hexColor 4", ()=>
+{
+    expect(functions.isHexColor("1")).toBe(false);
+})
+
+test("is hexColor 1", ()=>
+{
+    expect(functions.isDate("11/21/2022")).toBe(true);
+})
+
+test("is hexColor 2", ()=>
+{
+    expect(functions.isDate("05/14/2003")).toBe(true);
+})
+
+test("is hexColor 3", ()=>
+{
+    expect(functions.isDate("123@Gmail")).toBe(false);
+})
+
+test("is hexColor 4", ()=>
+{
+    expect(functions.isDate("1")).toBe(false);
+})
